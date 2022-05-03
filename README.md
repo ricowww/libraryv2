4daa5313727d5ef9b49fd3997df41905667f85f8
rico <rico.delapena.banzon@gmail.com>
2022-05-03 14:20:24 +0800

    ud

--- README.md
+++ README.md
@@ -23,3 +23,52 @@ You will need to associate your DOM elements with the actual book objects in som
 
 Add a button on each book’s display to change its read status.
 To facilitate this you will want to create the function that toggles a book’s read status on your Book prototype instance.
+
+
+CODE NOTES
+
+/* CHAPTER */
+//PLANNING
+//CODE
+//PRODUCTION
+
+/* PLANNING */
+//OUTPUTS
+/* 
+    Cards showing book details
+    Button inside cards to toggle read and unread
+    Cards can appear and disappear with button
+    Form to input new book 
+*/
+
+//INPUTS
+/* 
+    Book container = array
+    title = text
+    author = text
+    pages = number
+    status = yes/no
+    in library = yes/no 
+*/
+
+//STEPS
+/* 
+    Pressing button will show the form
+    User types book details
+    Computer counts nth number of book created
+    Book is created
+    Card will be created based on details
+    Read button will be attached to card
+    Remove button will be attached to card
+    Card will be attached to a card container
+    Card container will have classes which have linked styling
+ */
+
+///Pressing button will show the form
+/* 
+   DONE - Html button for the interface -
+   DONE - Js button object for the event listener
+   DONE - Event listener to trigger prompts, book ID number counter, and object constructor
+   DONE - Object constructor to create an book element inside books
+   DONE - Books object to collect all book elements
+*/
--- script_20220427.js
+++ script_20220427.js
@@ -1,10 +1,48 @@
+/* CHAPTER */
+//PLANNING
+//CODE
+//PRODUCTION
+
+/* PLANNING */
+//OUTPUTS
 /* 
-TO DO!!!!
-[ ] triggers
-[ ] grid format
-[ ] helpers
-[ ] id uniqueness
+    Cards showing book details
+    Button inside cards to toggle read and unread
+    Cards can appear and disappear with button
+    Form to input new book 
+*/
+
+//INPUTS
+/* 
+    Book container = array
+    title = text
+    author = text
+    pages = number
+    status = yes/no
+    in library = yes/no 
+*/
+
+//STEPS
+/* 
+    Pressing button will show the form
+    User types book details
+    Computer counts nth number of book created
+    Book is created
+    Card will be created based on details
+    Read button will be attached to card
+    Remove button will be attached to card
+    Card will be attached to a card container
+    Card container will have classes which have linked styling
  */
+
+///Pressing button will show the form
+/* 
+   DONE - Html button for the interface -
+   DONE - Js button object for the event listener
+   DONE - Event listener to trigger prompts, book ID number counter, and object constructor
+   DONE - Object constructor to create an book element inside books
+   DONE - Books object to collect all book elements
+*/
 
 /* OK - constructor function */
 function NewBook(title, author, pages, readStatus, bookId){
@@ -21,6 +59,18 @@ let bookIdCounter = 0;
 /* OK - contain book elements */
 const Books = [];
 
+/* OK - create new book by calling constructor and id counter */
+/* function promptNewBook() {
+    bookIdCounter = bookIdCounter + 1;
+    Books[prompt('Shorthand Name of Book')] = new NewBook(
+        prompt('Title'),
+        prompt('Author'),
+        prompt('Pages'),
+        prompt('Reading Status'),
+        bookIdCounter
+    )
+} */
+
 /* OK - define variables outside the functions to make them accessible via scope; use only these variables for card creation */
 let shortName = "";
 let bookTitle = "";
@@ -28,7 +78,7 @@ let bookAuthor = "";
 let bookPages = "";
 let readStatus ="";
 
-/* OK - create new book using constructor; also create a card */
+/* OK - version with intermediate variables for sharing between the constructor and the book card functions */
 function promptNewBook() {//get inputs via prompt for easy UI
     bookIdCounter = bookIdCounter + 1;
     shortName = prompt('Shorthand Name of Book');
@@ -46,14 +96,55 @@ function promptNewBook() {//get inputs via prompt for easy UI
     )
     //call the card maker
     newBookCard();
-}//Intermediate variables for sharing between the constructor and the book card functions
+}//Tested-Working up to NewBook and newBookCard
 
+/* OK - trigger create new book with button */
+const Make = document.querySelector("#make")
+Make.addEventListener('click', () =>{
+    promptNewBook();
+
+    }
+)
+
+/* DO - Trigger to read button */
+//declare button as an object
+//one.addEventListener('click', (e) => {typeToCalcVarMem(e.target)});
+//get id of target
+//call the read function
+
+
+/* DO - Trigger to delete button */
+
+///DO - User types book details
+/* 
+    helper for the following, if input is invalid, pop-up again the prompt
+        title = any
+        author = any
+        pages = nonzero positive integers only 
+        status = yes/no (read?) - dropdown or y/n
+        in library = yes/no (put in library?) dropdown or y/n
+ */
+
+///OK - Computer counts nth number of book created - see "pressing button..."
+
+///OK - Book is created - see "pressing button...."
+
+///Card will be created based on details
+/* 
+    An container will hold new cards
+    new cards will be created upon user input
+    new card will contain 
+        title
+        author
+        pages
+        status button
+        remove book button
+    status button will toggle yes/no
+    remove book button will delete the card
+*/
 
 /* OK - create attachment point thru #container */
 const Container = document.querySelector('#container');
-
-/* OK - container to hold all cards */
-const ButtonsContainer = [];
 
 /* OK -  Create book cards in html using DOM */
  function newBookCard(){
@@ -78,7 +169,7 @@ const ButtonsContainer = [];
     readStatusInMem.textContent = `Read: ${readStatus}`; //add content
     /* Append a delete button */
     let deleteButton = document.createElement('button');//deleteButton child is a new button in memory, variable is recyclable
-    deleteButton.id = 'delete'+ bookIdCounter; //book card id equal to delete + book ID
+    deleteButton.id = bookIdCounter; //book card id equal to book ID
     deleteButton.classList.add('delete');//delete child class for formatting purposes
     deleteButton.textContent = 'Delete'; //add content
     /* Append a removeread button */
@@ -86,6 +177,15 @@ const ButtonsContainer = [];
     readButton.id = bookIdCounter; //book card id equal to book ID
     readButton.classList.add('read');//read child class for formatting purposes
     readButton.textContent = 'Read'; //add content
+        //declare button as an object in DOM
+        //id-based event listener
+        //function to delete book DOM object and book array element
+
+    /* Append a read/unread button */
+        //declare button
+        //give an id
+        //id-based event listener
+        //function to change book read/unread status
     /* Append childs to book card */
     newDivInMem.appendChild(titleInMem); //attach child to div parent
     newDivInMem.appendChild(authorInMem); //attach child to div parent
@@ -93,15 +193,14 @@ const ButtonsContainer = [];
     newDivInMem.appendChild(readStatusInMem); //attach child to div parent
     newDivInMem.appendChild(deleteButton); //attach child to div parent
     newDivInMem.appendChild(readButton); //attach child to div parent
+
+
+
+        //attach delete button to div parent
+        //attach read button to div parent
+
     /* Append book card to container */
     Container.appendChild(newDivInMem); //attach child to div parent
-    /* DO - trigger create new book with button */
-    ButtonsContainer['delete'+ bookIdCounter] = document.getElementById('delete'+ bookIdCounter);//declare button as DOM named as "delete[insert id]"
-    ButtonsContainer['delete'+ bookIdCounter].addEventListener('click', (e) =>{
-        let idTemp = (e.target).id;
-        deleteBook(idTemp);
-        }
-    );
     
 }//code is working but was manually added to the call function 
 
@@ -136,30 +235,29 @@ function toggleRead(idForToggle){
     };
 };
 
+//read the reading status
+//if YES, go the container>card>read-child to the set read status to NO
+//also, change the container>card>read-button> to 'Read'
+//if NO, go the container>card>child set read status to YES
+//also, change the container>card>read-button> to 'Not Yet Read'
+
+//upon click, get the button id - DO
+/* trigger function */
+/* myButton.onclick = function() {
+    setUserName();
+  } */
 
 
-/* OK - trigger create new book with button */
-const Make = document.querySelector("#make")
-Make.addEventListener('click', () =>{
-    promptNewBook();
-    }
-)
 
-/* DO - Trigger to read button */
-//declare button as an object
-//one.addEventListener('click', (e) => {typeToCalcVarMem(e.target)});
-//get id of target
-//call the read function
-
-
-/* DO - Trigger to delete button */
-
-///DO - User types book details
+///Read button will be attached to card- see "Card will be created..."
+///Card will be attached to a card container- see "Card will be created..."
+///Card container will have classes which have linked styling
 /* 
-    helper for the following, if input is invalid, pop-up again the prompt
-        title = any
-        author = any
-        pages = nonzero positive integers only 
-        status = yes/no (read?) - dropdown or y/n
-        in library = yes/no (put in library?) dropdown or y/n
- */
+    structure is as follows: 
+        div#container> div.bookCard>
+            h1
+            p
+    div#container is a grid with at least 1 column,
+        infinite rows
+    bookCards have at least 200rem?px? width plus the extra space
+*/
